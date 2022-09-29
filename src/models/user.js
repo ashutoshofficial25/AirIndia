6f12d6a40d123dd5104375c5edf022553ea5b2f8
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-09-29 08:51:46 +0530

    ...auth

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const userSchema = new mongoose.Schema(
   {
@@ -18,6 +19,19 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async (next) => {
+  const user = this;
+  const hash = await bcrypt(this.password, 12);
+  this.password = hash;
+  next();
+});
+
+userSchema.method.isValidPassword = async (password) => {
+  const user = this;
+  const compare = await bcrypt.compare(password, this.password);
+  return compare;
+};
 
 const User = mongoose.model("User", userSchema);
 
---
+++ src/utils/auth.js
@@ -0,0 +1,3 @@
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const user = require("../models/user");
